32e791b3b8bf2ecec8757fdc2973dfe05ffb4dea
Kayode Adeniyi <kayode.adeniyi@andela.co>
2016-05-30 19:07:44 +0100

    Add category

---
+++ app/components/Category.react.js
@@ -0,0 +1,10 @@
+import 'category.styl'
+
+export default () => {
+  return(
+    <div className='category'>
+      <h1>The Caffeinator</h1>
+      <p>For the one that drinks the most coffee in a day</p>
+    </div>
+  )
+}
